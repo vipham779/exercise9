cbe44988940d18ab6a23b2216e3430cd24d6535f
Tuong Vi Pham <vipham779@gmail.com>
2020-06-15 07:55:38 -0700

    add ketqua

--- ketqua.py
+++ ketqua.py
@@ -10,29 +10,32 @@ import sys
 
 
 # return 
-def lottery_result(*args):
+def lottery_result():
     resp = requests.get('http://ketqua.net')
     tree = bs4. BeautifulSoup(resp.text, features='html.parser')
     table = tree.find('table', attrs={'id':'result_tab_md'}).find('tbody')
-    prizes = []
-    for line in table.find_all('tr'):
-        prize = line.find_all('td')
+    prizes = {}
+    for row in table.find_all('tr'):
+        prize = row.find_all('td')
         if len(prize) > 1:
-            for i in range(len(prize)):
-                prizes.append(prize[i].text)
+            for i in prize:
+                prizes.update({prize[0].text: [prize[i].text for i in range(1, len(prize))]})
     return prizes
 
 
-def display_lottery_result(input_data):
-    result = gamble(32, 10, 11)
+def verify_slots(input_data):
+    for key, value in prizes.items():
+        for num in value:
+            if int(num[-2:]) == input_data:
+                result = 'Win'
+            else:
+                result = 'Lose'
     return result
 
-
-def verify_slots()
-
-
 def main():
-    
+    if len(sys.argv) < 2:
+        print(lottery_result())
+    else:    
     
 
 if __name__ == '__main__':
